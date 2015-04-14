09785e0c99d1fca0691c2559501753cbf41da1dd
roguedesign <bromie9@hotmail.com>
2015-04-14 18:54:30 +1200

    added sql file

--- application/views/themes/default.php
+++ application/views/themes/default.php
@@ -11,6 +11,10 @@
 	<?php require 'application/views/head.php'; ?>
         <link href='http://fonts.googleapis.com/css?family=Dosis:300,400,700' rel='stylesheet' type='text/css'>
 
+        <link rel='stylesheet' href='assets/themes/default/css/bootstrap.css' type="text/css" >
+        <link rel='stylesheet' href='assets/themes/default/css/bootstrap.min.css' type='text/css' >
+        
+        
         <link rel="stylesheet" href="assets/themes/default/css/lightbox.css" type="text/css" >
     </head>
     <body>
@@ -19,7 +23,7 @@
             <div id="nav">
                 <ul>
                     <li>ABOUT US</li>
-                    <li>WHAT WE DO</li>
+                    
                     <li>PROJECTS</li>
                     <li>CONTACT US</li>
                 </ul>
@@ -35,14 +39,12 @@
                 <div id="about">
                     <br/><br/><br/><br/><br/><br/><br/><br/>
                     <h1>ABOUT US</h1>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
-                        id est laborum."
+                    <p>North South Construction LTD is a small local business that deals in Construction and light Architecture over the Christchuch region. 
+                        We have been operating for over 10 years and provide excellent service to customers and give excellent results. 
                     </p>
                     <br/><br/><br/><br/><br/><br/><br/><br/>
-                    <h1>WHAT WE DO</h1>
-                    <p>
+                    
+                    <p class="text">
                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
                         esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
@@ -59,17 +61,23 @@
                 </div>
             </div>
         
-        <div id="projects">
+        <div id="spacerimage">
+            
+        </div>
+        
+            <div class="projects">
             <br/><br/><br/><br/><br/><br/><br/><br/>
             
                 
                     <h1>PROJECTS</h1>
                     <br/><br/><br/>
                     
-                    
-                    <div id="project1">
-                        <h3>HOUSE IN SUMNER</h3>
-                        <img src="assets/themes/default/images/thumbs/houseview.jpg" alt="thumb of house view">
+                    <div class='container-fluid'>
+                    <div class='row'>
+                    <div class="-col-xs-6 col-md-4">
+                        <img class='thumb' src="assets/themes/default/images/thumbs/houseview.jpg" alt="thumb of house view">
+                        <h3>HOUSE IN SUMNER</h3>                        
+
                         <p>
                             This is a project we did up in the port hills overlooking Sumner, Christchurch. It is the first house we did from start to finish.
                         </p>
@@ -84,23 +92,46 @@
                         <a href="assets/themes/default/images/house/kitchenview.jpg" data-lightbox="house" data-title="KITCHEN VIEW"></a>
                     </div>
                     
-                    <br/>        
+                           
                
-                    <div id="project2">
-                            <h3>MOVIE THEATER</h3>
-                        <img src="assets/themes/default/images/thumbs/houseview.jpg" alt="thumb of house view">
+                    <div class="-col-xs-6 col-md-4">
+                        <img class='thumb' src="assets/themes/default/images/thumbs/houseview.jpg" alt="thumb of house view">
+                        <h3>MOVIE THEATER</h3>
                         <p>
-                            This is a project we did up in the port hills overlooking Sumner, Christchurch. It is the first house we did from start to finish.
+                            This was in Mt Cook at the Sir Edmund Hillary Museum. Building a movie theater with a sky dome.
                         </p>
                         <a href="assets/themes/default/images/movie/movie1.jpg" data-lightbox="movie" data-title="MOVIE THEATER">view</a>
                         <a href="assets/themes/default/images/movie/movie2.jpg" data-lightbox="movie" data-title="MOVIE"></a>
                         <a href="assets/themes/default/images/movie/movie3.jpg" data-lightbox="movie" data-title="MOVIE"></a>
                         <a href="assets/themes/default/images/movie/movie4.jpg" data-lightbox="movie" data-title="MOVIE THING"></a>
                         <a href="assets/themes/default/images/movie/movie5.jpg" data-lightbox="movie" data-title="MOOOOVIE"></a>
+                    </div> 
+                    
+                    
+                    
+                    <div class="-col-xs-6 col-md-4">
+                        <img class='thumb' src="assets/themes/default/images/thumbs/houseview.jpg" alt="thumb of house view">
+                        <h3>HOUSE IN SUMNER</h3>                        
+
+                        <p>
+                            This is a project we did up in the port hills overlooking Sumner, Christchurch. It is the first house we did from start to finish.
+                        </p>
+                       
+                        <a href="assets/themes/default/images/house/housebathroom.jpg" data-lightbox="house" data-title="BATHROOM">view</a>
+                        <a href="assets/themes/default/images/house/kitchen.jpg" data-lightbox="house" data-title="KITCHEN"></a>
+                        <a href="assets/themes/default/images/house/housebathroom3.jpg" data-lightbox="house" data-title="BATHROOM"></a>
+                        <a href="assets/themes/default/images/house/housedeck.jpg" data-lightbox="house" data-title="DECKING"></a>
+                        <a href="assets/themes/default/images/house/houseside.jpg" data-lightbox="house" data-title="SIDE"></a>
+                        <a href="assets/themes/default/images/house/houseside2.jpg" data-lightbox="house" data-title="SIDE"></a>
+                        <a href="assets/themes/default/images/house/houseview.jpg" data-lightbox="house" data-title="VIEW"></a>
+                        <a href="assets/themes/default/images/house/kitchenview.jpg" data-lightbox="house" data-title="KITCHEN VIEW"></a>
                     </div>
-            
-        </div>
+                    </div>
+                    </div>                 
+            </div>
         <script src="assets/themes/default/js/jquery-1.11.0.min.js"></script>
 	<script src="assets/themes/default/js/lightbox.min.js"></script>
+        <script src='assets/themes/default/js/bootstrap.js'></script>
+        <script src='assets/themes/default/js/bootstrap.min.js'></script>
     </body>
 </html>
